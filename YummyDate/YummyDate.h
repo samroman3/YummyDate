ce018f3d58d7465d9a5ea61e8d324b0331bb98a5
Samuel Roman <samroman3@gmail.com>
2024-04-13 19:01:51 -0400

    Initial Commit

---
+++ YummyDate/YummyDate.h
@@ -0,0 +1,18 @@
+//
+//  YummyDate.h
+//  YummyDate
+//
+//  Created by Sam Roman on 4/13/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YummyDate.
+FOUNDATION_EXPORT double YummyDateVersionNumber;
+
+//! Project version string for YummyDate.
+FOUNDATION_EXPORT const unsigned char YummyDateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YummyDate/PublicHeader.h>
+
+
